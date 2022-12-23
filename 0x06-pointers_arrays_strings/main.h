47534d0cafd3353fd7f2416a46717f75f939d482
handrew2022 <andrewosasuyi@gmail.com>
2022-12-23 09:08:23 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
 char *_strncat(char *dest, char *src, int n);
 
 #endif
