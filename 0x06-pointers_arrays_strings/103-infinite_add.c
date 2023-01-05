82aea497dcd5caa58203408016ba559bddc784e6
handrew2022 <andrewosasuyi@gmail.com>
2023-01-04 16:43:49 -0800

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j++, i--)
+	{
+		i++;
+	}
+	i-- ;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + 1) = temp;
+	}
+}
+infinite_add - add 2 numbers together
+
